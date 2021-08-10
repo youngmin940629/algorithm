371a748c86f2b6f2fd7f4ea6a9f23599091a4266
YoungMin <kgjdudals@naver.com>
2021-08-10 18:02:55 +0900

    전기버스 변수수정

--- 0810/12385_배열1_전기버스_확인용.py
+++ 0810/12385_배열1_전기버스_확인용.py
@@ -6,24 +6,23 @@
 case = int(input())
 cnt = 1
 while cnt <= case:
-    data = list(map(int, input().split()))
+    K, M, N = map(int, input().split())
     data_list = list(map(int, input().split()))
-    count_list = [0] * (data[1] + 1)
+    count_list = [0] * (M + 1)
     for count_idx in data_list:
         count_list[count_idx] += 1
     stop_idx = 0
     stop_cnt = 0
     not_charge = 0
-    while stop_idx + data[0] < data[1]:
-        for idx in range(stop_idx + data[0], stop_idx, -1):
+    while stop_idx + K < M:
+        for idx in range(stop_idx + K, stop_idx, -1):
             if count_list[idx] == 1:
                 stop_idx = idx
                 stop_cnt += 1
                 break
             if idx == stop_idx + 1:
-                count_list == 0
                 stop_cnt = 0
-                stop_idx += data[0]
+                stop_idx += K
                 not_charge = 1
                 break
         if not_charge == 1:
