d862867bd039ecae2561edfb3498888ae61159d2
YoungMin <kgjdudals@naver.com>
2021-08-12 15:23:13 +0900

    부분집합

--- 0812/4837_부분집합의합.py
+++ 0812/4837_부분집합의합.py
@@ -1,5 +1,14 @@
 for tc in range(int(input())):
     N, K = map(int, input().split())
-    A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     cnt = 0
+    for i in range(1, 1<<12):
+        bit_cnt = 0
+        num_sum = 0
+        for j in range(12):
+            if i & (1 <<j):
+                num_sum += j + 1
+                bit_cnt += 1
+        if num_sum ==K and bit_cnt == N:
+            cnt += 1
+
     print('#{}'.format(tc + 1), cnt)
