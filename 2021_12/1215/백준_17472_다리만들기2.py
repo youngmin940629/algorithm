38ecb2962a2c3edc444f429977d5556c94774aa9
YoungMin <kgjdudals@naver.com>
2021-12-18 20:36:10 +0900

    다리만들기

--- 2021_12/1215/백준_17472_다리만들기2.py
+++ 2021_12/1215/백준_17472_다리만들기2.py
@@ -13,12 +13,30 @@ def bfs():
                     visited.append((nx,ny))
     island_num += 1
 
+def bridge(st_land, x, y, dir):
+    bridge_distance = 0
+    while True:
+        x, y = x + dx[dir], y + dy[dir]
+        if 0 <= x < N and 0 <= y < M:
+            if arr[x][y] == 0:
+                bridge_distance += 1
+                continue
+            elif arr[x][y] == st_land:
+                return
+            else:
+                if bridge_distance >= 2 and island_bridge[st_land][arr[x][y]] > bridge_distance:
+                    island_bridge[st_land][arr[x][y]] = island_bridge[arr[x][y]][st_land] = bridge_distance
+                return
+        else:
+            return
+
+
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 island_num = 1
 q = deque()
-edge = []
 visited = []
+result = 0
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 for x in range(N):
@@ -27,4 +45,34 @@ for x in range(N):
             q.append((x,y))
             visited.append((x,y))
             bfs()
-print(arr)
+island_bridge = [[0xffffff] * (island_num) for _ in range(island_num)]
+for x in range(N):
+    for y in range(M):
+        if arr[x][y] != 0:
+            for direction in range(4):
+                bridge(arr[x][y], x, y, direction)
+edge = []
+for x in range(island_num):
+    for y in range(x+1, island_num):
+        if island_bridge[x][y] != 0xffffff:
+            edge.append((island_bridge[x][y], x, y))
+edge.sort()
+cnt = 0
+p = [i for i in range(island_num)]
+
+def find_set(x):
+    while x != p[x]:
+        x = find_set(p[x])
+    return p[x]
+
+for w, u, v in edge:
+    if find_set(u) != find_set(v):
+        cnt += 1
+        result += w
+        p[find_set(v)] = find_set(u)
+        if cnt == island_num - 2:
+            break
+if cnt != island_num -2:
+    print(-1)
+else:
+    print(result)
