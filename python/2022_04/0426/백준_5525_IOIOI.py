18b9712f3fb18a260c00bc8841fbb767e12c4cc9
youngmin <kgjdudals@naver.com>
2022-04-26 19:49:03 +0900

    algorithm

--- python/2022_04/0426/백준_5525_IOIOI.py
+++ python/2022_04/0426/백준_5525_IOIOI.py
@@ -9,10 +9,10 @@ result = 0
 while i < M-1:
     if S[i:i+3] == 'IOI':
         count += 1
+        i += 2
         if count == N:
             result += 1
             count -= 1
-        i += 2
     else:
         count = 0
         i += 1
