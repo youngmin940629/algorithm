5f369e4a04f94cc22438984d8a650484e2ca8906
YoungMin <kgjdudals@naver.com>
2022-06-25 10:45:23 +0900

    algorithm

---
+++ python/2022_06/0625/백준_89801_N-Queen.py
@@ -0,0 +1,22 @@
+def queen(visit, x):
+    global cnt
+    if x == N:
+        cnt += 1
+        return
+    for col in range(N):
+        visit[x] = col
+        for i in range(x):
+            if visit[i] == visit[x]:
+                break
+            if abs(visit[i] - visit[x]) == (x-i):
+                break
+        else:
+            queen(visit, x+1)
+
+
+
+N = int(input())
+cnt = 0
+visit = [0] * N
+queen(visit, 0)
+print(cnt)
