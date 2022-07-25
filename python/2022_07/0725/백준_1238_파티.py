bd46887efda7426380adbefa8baa35bbdf82fde9
YoungMin <kgjdudals@naver.com>
2022-07-25 19:26:47 +0900

    algorithm

---
+++ python/2022_07/0725/백준_1238_파티.py
@@ -0,0 +1,36 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+INF = 0xffffff
+v, e, x = map(int, input().split())
+graph = [[] for _ in range(v + 1)]
+
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    graph[a].append((b, cost))
+
+
+def dijkstra(start):
+    q = []
+    distance = [INF] * (v + 1)
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for node_index, node_cost in graph[now]:
+            cost = dist + node_cost
+            if distance[node_index] > cost:
+                distance[node_index] = cost
+                heapq.heappush(q, (cost, node_index))
+    return distance
+
+result = 0
+for i in range(1, v + 1):
+    go = dijkstra(i)
+    back = dijkstra(x)
+    result = max(result, go[x] + back[i])
+
+print(result)
